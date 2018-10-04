9f82059fbe3975ce5e8a167cee7fa99fcccfd8a2
lasantha <lasanthaslakmal@gmail.com>
2018-10-04 17:44:19 +0530

    added test file and tesing library module

---
+++ test/index.spec.js
@@ -0,0 +1,13 @@
+const assert = require('assert');
+
+describe('index Test', function () {
+    
+    it('findCountry(): should return Country object', function () {
+        assert.equal("Hello".length, 5);
+    });
+
+    it('findISO2Code(): should return ISO2 Code', function () {
+        assert.equal("Hello".charAt(0), 'H');
+    });
+
+});
